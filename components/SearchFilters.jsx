b7ccf8b96f6183499543c8df7188d0a2990c2e5d
Kennedy Gitahi <kengitahi@hotmail.com>
2022-03-08 19:03:09 +0300

    Finish for sale and for rent property list pages

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,11 @@
+import {useEffect, useState} from "react"
+import {Flex, Select, Box, Text, Input, Spiner, Icon, Button} from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import {MdCancel} from "react-icons/md"
+import Image from "next/image"
+
+export default function SearchFilters(){
+    return(
+        <h1>Search Filters</h1>
+    )
+}
